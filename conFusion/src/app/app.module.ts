79dfad4b5afae8c20b91f4ec0535a19d4f44234f
ArthurMGoulart <budiarthur@gmail.com>
2022-06-03 10:53:10 -0300

    feat: dishService

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -7,13 +7,21 @@ import { MatCardModule } from '@angular/material';
 import { MatButtonModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { MatGridListModule } from '@angular/material';
+import { MatListModule } from '@angular/material';
+
 import 'hammerjs';
+
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
+
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
@@ -21,10 +29,11 @@ import { MenuComponent } from './menu/menu.component';
     MatToolbarModule,
     FlexLayoutModule,
     MatGridListModule,
+    MatListModule,
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
