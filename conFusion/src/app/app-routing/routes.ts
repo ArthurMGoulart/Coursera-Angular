312ae815ca70919c0971d870d2aa5eb1b1c81a0f
ArthurMGoulart <budiarthur@gmail.com>
2022-06-05 18:44:30 -0300

    feat: dishDetail routing

--- conFusion/src/app/app-routing/routes.ts
+++ conFusion/src/app/app-routing/routes.ts
@@ -9,6 +9,7 @@ import { DishdetailComponent } from '../dishdetail/dishdetail.component';
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'menu', component: MenuComponent},
+  {path: 'dishdetail/:id', component: DishdetailComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'about', component: AboutComponent},
   {path: 'contactus', component: ContactComponent}
