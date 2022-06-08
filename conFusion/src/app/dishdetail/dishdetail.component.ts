463fcda02e7197a9ca2d2e1d907c692e5470e444
ArthurMGoulart <budiarthur@gmail.com>
2022-06-08 11:14:12 -0300

    feat: adding put method to dish comments

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -22,6 +22,7 @@ export class DishdetailComponent implements OnInit {
   commentForm: FormGroup;
   currComment: Comment;
   @ViewChild('fform') commentFormDirective;
+  dishcopy: Dish;
 
   formErrors = {
     'rating': '',
@@ -57,6 +58,7 @@ export class DishdetailComponent implements OnInit {
       .pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
       .subscribe((dish) => {
         this.dish = dish;
+        this.dishcopy = dish;
         this.setPrevNext(dish.id);
       },
       (errmess) => this.errMess = <any>errmess);
@@ -100,14 +102,23 @@ export class DishdetailComponent implements OnInit {
       ...this.commentForm.value,
       date: new Date()
     };
-    this.dish.comments.push(this.currComment);
-    this.currComment = null;
+    this.dishcopy.comments.push(this.currComment);
+    this.dishService.putDish(this.dishcopy)
+      .subscribe((dish) => {
+        this.dish = dish;
+        this.dishcopy = dish
+      },
+      (errmess) => {
+        this.dish = null;
+        this.dishcopy = null;
+        this.errMess = <any>errmess;
+      });
+    this.commentFormDirective.resetForm();
     this.commentForm.reset({
       author: '',
       comment: '',
       rating: 5
     });
-    this.commentFormDirective.resetForm();
   }
 
   setPrevNext(dishId: string) {
